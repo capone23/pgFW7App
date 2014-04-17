0a70278d19b8b710455adca4cd557be9c592c1e4
Vlad Plaz <vlad23@gmail.com>
2014-04-17 13:06:23 -0400

    final commit app complete ready for testflight and HUGE

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,7 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/nl.x-services.plugins.calendar/www/Calendar.js",
+        "id": "nl.x-services.plugins.calendar.Calendar",
+        "clobbers": [
+            "Calendar"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "nl.x-services.plugins.calendar": "4.2.2"
+}
 // BOTTOM OF METADATA
 });
